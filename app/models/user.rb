d2c0e337f58f96210470b52220ad3d5cb71bdbfc
Drew Reynolds <NYDrewReynolds@gmail.com>
2015-08-10 11:57:34 -0600

    End of lesson

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
   has_many :orders
+
+  after_create  :clear_cache
+  after_save    :clear_cache
+  after_destroy :clear_cache
+
+  def clear_cache
+    Rails.cache.clear
+  end
 end
